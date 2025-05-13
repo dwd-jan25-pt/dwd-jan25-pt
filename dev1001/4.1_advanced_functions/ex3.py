4bb6d75b470a79cbd9338557084832d5ffbd2629
Mr. Robot <flynnwebdev@gmail.com>
2025-05-13 10:00:14 +1000

    Lesson 4.1 completed up to 4a_map

---
+++ dev1001/4.1_advanced_functions/ex3.py
@@ -0,0 +1,14 @@
+# Lambdas Exercise
+# 
+# You have a list of tuples.
+# Use the built-in sorted() function with a lambda as the 'key' argument
+#   to sort these points on their y-coordinate (2nd element of each tuple).
+# Print out the sorted points.
+
+points = [(1, 2), (3, 1), (5, -4), (0, 0)]
+
+# Can abbreviate lambda param to 'p' because of the context (inline)
+# In any other context, use a more descriptive name
+sorted_points = sorted(points, key=lambda p: p[1])
+
+print(sorted_points)
